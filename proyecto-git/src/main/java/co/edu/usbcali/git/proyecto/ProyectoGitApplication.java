51e3e6b1fefcd216f17b5bd018cc46f5665c7c0c
leoromsi <leonardo.romero@carvajal.com>
2020-11-14 09:12:38 -0500

    6to commit / proyecto spring

---
+++ proyecto-git/src/main/java/co/edu/usbcali/git/proyecto/ProyectoGitApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usbcali.git.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGitApplication.class, args);
+	}
+
+}
---
+++ proyecto-git/src/test/java/co/edu/usbcali/git/proyecto/ProyectoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usbcali.git.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
